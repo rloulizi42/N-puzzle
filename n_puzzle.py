71da6ba8471caa0c0310366ddf07f23dce52cc7f
Rayan LOULIZI <rloulizi@e1r5p18.42.fr>
2019-04-08 19:41:38 +0200

    first A* implementation

--- n_puzzle.py
+++ n_puzzle.py
@@ -8,10 +8,6 @@ n = data[1]
 data = [list(x.replace(" ", "")) for x in data[2:]]
 data = [list(map(int, x)) for x in data]
 
-solved = [[1, 2, 3],
-          [8, 0, 4],
-          [7, 6, 5]]
-
 DOWN = (-1, 0)
 UP = (1, 0)
 LEFT = (0, 1)
@@ -20,9 +16,13 @@ RIGHT = (0, -1)
 def can_move(dir, grid, x1, y1):
     y = y1 + dir[0]
     x = x1 + dir[1]
-    if y < 0 or x < 0 or x > len(grid) or y > len(grid):
+    if y < 0 or x < 0 or x >= len(grid) or y >= len(grid):
         return 0
     return 1
+
+def printer(grid):
+    for line in grid:
+        print(line)
 
 def move(dir, grid):
     grid_all = []
@@ -40,10 +40,6 @@ def move(dir, grid):
         grid_all[y1 + dir[0]][x1 + dir[1]] = 0
     return grid_all
 
-def printer(grid):
-    for line in grid:
-        print(line)
-
 def getChild(grid):
     childs = []
     for m in [DOWN, UP, LEFT, RIGHT]:
@@ -54,61 +50,87 @@ def getChild(grid):
 
 
 class Node():
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, grid=None):
         self.parent = parent
-        self.grid = []
+        self.grid = grid
         
         self.g = 0
         self.h = 0
         self.f = 0
 
-mv_list = []
+    def __lt__(self, other):
+        if(self.f < other.f):
+            return True
+        else:
+            return False
+
+def getCoord(solved, value, b):
+    for y, v in enumerate(solved):
+        for x, v1 in enumerate(v):
+            if value == v1:
+                if b:
+                    return y
+                else:
+                    return x
+    return 0
 
 def h(grid,solved):
     result = 0
     for y, v in enumerate(grid):
         for x, value in enumerate(v):
             if grid[y][x] != solved[y][x] and value == 0:
-                result += 1
+                result += abs(y - getCoord(solved, grid[y][x], True)) + abs(x - getCoord(solved, grid[y][x], False))
     return result
 
-def g(grid)
+def reconstruct_path(node):
+    res = []
+    while node.parent != None:
+        res.insert(0, node.grid)
+        node = node.parent
+    return res
 
 def solve(data, solved):
-    Node start = data.
-    open_set = [data]
+    start = Node(None, data)
+    open_set = []
     closed_set = []
-    heapify(open_set)
-    heapify(closed_set)
+    heappush(open_set, start)
     while open_set:
+        push = True
         current = heappop(open_set)
-        heappush(closed_set, current)
-        if current == solved:
-      #     return
-        for child in getChild(current):
-            curr_s = Node(child)
-            if curr_s in closed_set:
+        closed_set.insert(0, current)
+        if current.grid == solved:
+            path = reconstruct_path(current)
+            path.insert(0,data)
+            return path
+        for child in getChild(current.grid):
+            child_node = Node(current, child)
+            if child_node in closed_set:
+                continue
+            child_node.g = current.g + 1
+            child_node.h = h(child, solved)
+            child_node.f = child_node.g + child_node.h
+            for open_node in open_set:
+                if child == open_node.grid:
+                    push = False
+                    if child_node.f < open_node.f:
+                        open_node.g = child_node.g
+                        open_node.h = child_node.h
+                        open_node.f = child_node.f
+                        open_node.parent = child_node.parent
+            if push:
+                heappush(open_set, child_node)
+    print('No Solution')
                 
-#        if (curr == solved):
-#            reconstruct_path
-    
-#solve(data, solved)
-#a = Node()
-#a.h = 42
-#b = Node()
-#b.h = 21
+solved = [[1, 2, 3],
+          [8, 0, 4],
+          [7, 6, 5]]
 
-heap = []
-#heappush(heap, (a.h, a))
-#heappush(heap, (b.h, b))
-#while heap:
- #    print(heappop(heap)[0])
+data1 =  [[1, 2, 3],
+          [7, 8, 4],
+          [0, 6, 5]]
 
-print('initial')
-printer(solved)
-print('')
+c = solve(data, solved)
 
-c = getChild(solved)
-for chil in c:
-    printer(chil)
-    print('\n')
+for e in c:
+    print('')
+    printer(e)
