23b285ce571efb41a0b3fdeb6cb8e7353f7ea615
Rayan LOULIZI <rloulizi@e1r5p18.42.fr>
2019-04-11 19:53:50 +0200

    improve algo by add heuristic

--- n_puzzle.py
+++ n_puzzle.py
@@ -1,5 +1,6 @@
 import sys
 from heapq import heappop, heappush, heapify
+import math
 
 with open(sys.argv[1]) as f:
     data = f.readlines()
@@ -13,31 +14,33 @@ UP = (1, 0)
 LEFT = (0, 1)
 RIGHT = (0, -1)
 
-def can_move(dir, grid, x1, y1):
-    y = y1 + dir[0]
-    x = x1 + dir[1]
-    if y < 0 or x < 0 or x >= len(grid) or y >= len(grid):
-        return 0
-    return 1
+solved = [[1, 2, 3],
+          [8, 0, 4],
+          [7, 6, 5]]
 
+flat_solved = [n for lists in solved for n in lists]
+dictOfGrid = {flat_solved[i] : i for i in range(0,len(flat_solved))}
+n = math.sqrt(len(flat_solved))
 def printer(grid):
     for line in grid:
         print(line)
 
 def move(dir, grid):
     grid_all = []
-    y1 = x1 = 0
+    y1 = x1 = yy = xx = 0
     for y, v in enumerate(grid):
         grids = []
         for x, value in enumerate(v):
             if value == 0:
-                y1 = y
-                x1 = x
+                y1 = y + dir[0]
+                x1 = x + dir[1]
+                yy = y
+                xx = x
             grids.append(value)
         grid_all.append(grids)
-    if can_move(dir, grid, x1, y1):
-        grid_all[y1][x1] = grid[y1 + dir[0]][x1 + dir[1]]
-        grid_all[y1 + dir[0]][x1 + dir[1]] = 0
+    if not (y1 < 0 or x1 < 0 or x1 >= n or y1 >= n):
+        grid_all[yy][xx] = grid[y1][x1]
+        grid_all[y1][x1] = 0
     return grid_all
 
 def getChild(grid):
@@ -50,7 +53,7 @@ def getChild(grid):
 
 
 class Node():
-    def __init__(self, parent=None, grid=None):
+    def __init__(self, parent=None, grid=None, f=0):
         self.parent = parent
         self.grid = grid
         
@@ -58,28 +61,36 @@ class Node():
         self.h = 0
         self.f = 0
 
+    def __hash__(self):
+        e = [n for lists in self.grid for n in lists]
+        return hash(tuple(e))
+
+    def __ne__(self, other):
+        if self is None or other is None:
+            return True
+        if self.grid == other.grid:
+            return False
+        else:
+                return True
+
+    def __eq__(self, other):
+        if self.grid == other.grid:
+            return True
+        else:
+                return False
+
     def __lt__(self, other):
         if(self.f < other.f):
             return True
         else:
             return False
 
-def getCoord(solved, value, b):
-    for y, v in enumerate(solved):
-        for x, v1 in enumerate(v):
-            if value == v1:
-                if b:
-                    return y
-                else:
-                    return x
-    return 0
-
 def h(grid,solved):
     result = 0
     for y, v in enumerate(grid):
         for x, value in enumerate(v):
-            if grid[y][x] != solved[y][x] and value == 0:
-                result += abs(y - getCoord(solved, grid[y][x], True)) + abs(x - getCoord(solved, grid[y][x], False))
+            if grid[y][x] != solved[y][x] and value != 0:
+                result += abs(y - dictOfGrid[value] // n) + abs(x - dictOfGrid[value] % n)
     return result
 
 def reconstruct_path(node):
@@ -91,19 +102,17 @@ def reconstruct_path(node):
 
 def solve(data, solved):
     start = Node(None, data)
-    open_set = []
-    closed_set = []
-    closedMap_set = set(closed_set)
-    openMap_set = set(open_set)
-    heappush(open_set, start)
-    while open_set:
-        current = heappop(open_set)
-        closed_set.insert(0, current)
+    open_l = []
+    closed = []
+    closedMap_set = set(closed)
+    openMap_set = {start.__hash__: start}
+    heappush(open_l, start)
+    while open_l:
+        current = heappop(open_l)
+        closed.append(current)
         closedMap_set.add(current)
         if current.grid == solved:
-            path = reconstruct_path(current)
-            path.insert(0,data)
-            return path
+            return reconstruct_path(current)
         for child in getChild(current.grid):
             child_node = Node(current, child)
             if child_node in closedMap_set:
@@ -111,22 +120,22 @@ def solve(data, solved):
             child_node.g = current.g + 1
             child_node.h = h(child, solved)
             child_node.f = child_node.g + child_node.h
-            if child_node in openMap_set and child_node.f < open_node.f:
-                continue
-            heappush(open_set, child_node)
-            openMap_set.add(child_node)
+
+            if child_node in openMap_set.values():
+                tmp = openMap_set[child_node.__hash__]
+                if child_node.f < tmp.f:
+                    tmp.parent = child_node.parent
+                    tmp.f = child_node.f
+                    tmp.g = child_node.g
+                    tmp.h = child_node.h
+                    continue
+            heappush(open_l, child_node)
+            openMap_set.update({child_node.__hash__:child_node})
     print('No Solution')
                 
-solved = [[1, 2, 3],
-          [8, 0, 4],
-          [7, 6, 5]]
-
 data1 =  [[1, 2, 3],
           [7, 8, 4],
           [0, 6, 5]]
 
-c = solve(data, solved)
 
-for e in c:
-    print('')
-    printer(e)
+c = solve(data, solved)
